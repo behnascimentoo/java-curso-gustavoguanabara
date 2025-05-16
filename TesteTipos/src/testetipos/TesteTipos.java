f778bb7fff51a23de24164bcb771f4eb431c00aa
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-05-16 14:58:55 -0300

    commit tipos primitivos

---
+++ TesteTipos/src/testetipos/TesteTipos.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testetipos;
+
+/**
+ *
+ * @author Beh
+ */
+public class TesteTipos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* int idade = 30;
+        String valor = Integer.toString(idade); // converte numero para string
+        System.out.println(valor);*/
+        String valor = "30.5";
+        float idade = Float.parseFloat(valor); // converte string para inteiro
+        System.out.println(idade);
+    }
+    
+}
