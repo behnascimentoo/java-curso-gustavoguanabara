79c007572282f4eae92e60105ee4a2133bea377b
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-05-21 13:27:44 -0300

    commit exercicio for

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author Beh
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc=0; cc<=100; cc+=10) { // cc+=a vai pulando de 2 em 2, cc-=2 vai voltando de 2 em 2
+            System.out.println("Cambalhota" + cc);
+        }
+    }
+    
+}
