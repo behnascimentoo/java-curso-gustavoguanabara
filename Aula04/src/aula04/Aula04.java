c7b5e07125336e53aece19d11d574f37ca689367
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-05-22 17:28:05 -0300

    commit aula04

---
+++ Aula04/src/aula04/Aula04.java
@@ -0,0 +1,11 @@
+
+package aula04;
+public class Aula04 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC",0.4f, "Amerela");
+        
+        Caneta c2 = new Caneta("KKK", 1.5f, "Laranja");
+        
+    }
+    
+}
