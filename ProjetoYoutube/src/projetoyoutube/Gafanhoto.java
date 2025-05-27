7a786e0e139503bac79894462000ac0a3dd1edb9
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-05-27 20:44:31 -0300

    Projeto Youtube

---
+++ ProjetoYoutube/src/projetoyoutube/Gafanhoto.java
@@ -0,0 +1,41 @@
+package projetoyoutube;
+public class Gafanhoto extends Pessoa {
+    private String login;
+    private int totAssistido;
+    //Construtor
+    public Gafanhoto(String nome, int idade, String sexo, String login) {
+        super(nome, idade, sexo);
+        this.login = login;
+        this.totAssistido = 0;
+    }
+    
+    //Metodos
+    public void viuMaisUm() {
+        this.totAssistido++;
+    }
+    
+    //to string
+    @Override
+    public String toString() {
+        return "Gafanhoto{" + super.toString() + "\nlogin=" + login + ", totAssistido=" + totAssistido + '}';
+    }
+    
+    
+    //Metodos Especiais
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistido() {
+        return totAssistido;
+    }
+
+    public void setTotAssistido(int totAssistido) {
+        this.totAssistido = totAssistido;
+    }
+    
+}
